30c5048b2e8df128d0d82a20bf1e2fe9d7ab99e6
JonasDriessen0 <114576261+JonasDriessen0@users.noreply.github.com>
2023-11-29 14:41:30 +0100

    Texting map updated and other fixes and improvements

--- ProjectUltrakill/Assets/Developers/Jonas/Scripts/Shoot.cs
+++ ProjectUltrakill/Assets/Developers/Jonas/Scripts/Shoot.cs
@@ -6,6 +6,10 @@ public class Shoot : MonoBehaviour
 {
     public float damage = 10f;
     public float range = 100f;
+    public float shootingCooldown = 0.1f;
+
+    private bool canShoot = true;
+    private float timeSinceLastShot = 0f;
 
     public bool TrailTrue = false;
 
@@ -14,10 +18,11 @@ public class Shoot : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetButtonDown("Fire1"))
+        if (canShoot && Input.GetButton("Fire1"))
         {
             shoot();
         }
+
         if (Input.GetButtonDown("Fire2"))
         {
             Debug.Log("charge");
@@ -26,25 +31,36 @@ public class Shoot : MonoBehaviour
         {
             Debug.Log("shoot");
         }
+
+        timeSinceLastShot += Time.deltaTime;
+        if (timeSinceLastShot >= shootingCooldown)
+        {
+            canShoot = true;
+        }
     }
 
     void shoot()
     {
-        TrailTrue = true;
-        animator.SetTrigger("ShootTrigger");
-        RaycastHit hit;
-        if(Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range))
+        if (canShoot)
         {
-            if (hit.collider.CompareTag("EnemyTest"))
+            TrailTrue = true;
+            animator.SetTrigger("ShootTrigger");
+            RaycastHit hit;
+            if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range))
             {
-                Debug.Log("Enemy hit");
-                Destroy(hit.collider.gameObject);
+                if (hit.collider.CompareTag("EnemyTest"))
+                {
+                    Debug.Log("Enemy hit");
+                    Destroy(hit.collider.gameObject);
+                }
+                else
+                {
+                    Debug.Log("Enemy not hit");
+                }
+            }
 
-            }
-            else
-            {
-                Debug.Log("Enemy not hit");
-            }
+            timeSinceLastShot = 0f;
+            canShoot = false;
         }
     }
 }
