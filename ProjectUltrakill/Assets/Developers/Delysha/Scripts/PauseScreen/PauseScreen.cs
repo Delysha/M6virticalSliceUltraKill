62381176d8b35c45e21c9199c0916054d98e362f
Delysha <114736055+Delysha@users.noreply.github.com>
2024-01-30 12:20:48 +0100

    PauseScreen

---
+++ ProjectUltrakill/Assets/Developers/Delysha/Scripts/PauseScreen/PauseScreen.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseScreen : MonoBehaviour
+{
+
+    public  bool GameIsPaused = false;
+
+    public GameObject PauseScreenUI;
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GameIsPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        PauseScreenUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    }
+
+    void Pause()
+    {
+        Cursor.lockState = CursorLockMode.None;
+        PauseScreenUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
+    
+    public void ReturnToStart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+    }
+}
