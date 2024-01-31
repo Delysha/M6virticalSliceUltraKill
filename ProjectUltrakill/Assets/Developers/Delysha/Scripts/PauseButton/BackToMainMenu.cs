98fa37a7a405a1bb18960ac9573c1e791d181eca
Seinaru <34807@ma-web.nl>
2024-01-31 17:52:00 +0100

    Attempting PauseScreen

---
+++ ProjectUltrakill/Assets/Developers/Delysha/Scripts/PauseButton/BackToMainMenu.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BackToMainMenu : MonoBehaviour
+{
+    public void BackToMainScene()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
---
+++ ProjectUltrakill/Assets/Developers/Delysha/Scripts/PauseButton/PauseScreen.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseScreen : MonoBehaviour
+{
+
+    public bool GameIsPaused = false;
+
+    public GameObject PauseScreenUI;
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GameIsPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        PauseScreenUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    }
+
+    void Pause()
+    {
+        Cursor.lockState = CursorLockMode.None;
+        PauseScreenUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
+
+    public void LevelRestart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
